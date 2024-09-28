2d77c3353c991af1aa31779446a6b4ad10809cec
SimSimi Education Company <sashanurmi@hotmail.com>
2024-09-29 02:27:40 +0300

    Remove license.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,1 @@
-/* Copyright © 2024 SimSimi Inc.
-This is a designed by MooGPT application for Android.
-Please what's the learn more.
-*/
-
 implementation 'com.google.android.material:material:1.12.0'
