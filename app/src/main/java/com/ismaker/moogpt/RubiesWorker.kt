93c3ff409c292ea89db5cd65c9784e75167eda36
SimSimi Education Company <sashanurmi@hotmail.com>
2024-09-29 02:31:43 +0300

    Remove license.

--- app/src/main/java/com/ismaker/moogpt/RubiesWorker.kt
+++ app/src/main/java/com/ismaker/moogpt/RubiesWorker.kt
@@ -1,8 +1,3 @@
-/* Copyright © 2024 SimSimi Inc.
-This is a designed by MooGPT application for Android.
-Please what's the learn more.
-*/
-
 class RubiesWorker(appReference: reference, workerParams: WorkerParameters):
     Worker(appReference, workerParams) {
 
